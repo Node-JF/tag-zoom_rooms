9c3be67af7104747877e374e6e6a0e41025011cb
Jason Foord <jf@tag.com.au>
2022-11-07 15:40:34 +1030

    expose current booking control

--- src/info.lua
+++ src/info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "TAG~Zoom~Zoom Rooms CLI",
   Version = "1.10",
-  BuildVersion = "1.10.0.16",
+  BuildVersion = "1.11.0.0",
   Id = "c446ef61-0b8d-41bb-b354-d24e35165a2d",
   Description = "Zoom Rooms JSON CLI with support for massive phonebooks, contacts searching & filtering, contact presence updates, HTML5 compatible listboxes, host ending call flag options, custom plugin settings",
   Author = "Jason Foord",
